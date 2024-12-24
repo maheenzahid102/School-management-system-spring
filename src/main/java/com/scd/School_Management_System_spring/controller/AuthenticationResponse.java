c1dc106d03d0b94e7ad79b1ef575e0d9a1c0837d
Muntahakashif <muntahakashif2004@gmail.com>
2024-12-24 16:39:13 +0500

    remove src

--- src/main/java/com/scd/School_Management_System_spring/controller/AuthenticationResponse.java
+++
@@ -1,6 +0,0 @@
-package com.scd.School_Management_System_spring.controller;
-
-public class AuthenticationResponse {
-    public AuthenticationResponse(String jwt) {
-    }
-}
--- src/main/java/com/scd/School_Management_System_spring/enums/UserRole.java
+++
@@ -1,7 +0,0 @@
-package com.scd.School_Management_System_spring.enums;
-
-public enum UserRole {
-    admin
-
-
-}
